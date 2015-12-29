834458c49b235d201920b0cf3dd9d113ed94ba74
AlexSchechter <a.schechter@outlook.com>
2015-12-29 18:29:41 +0000

    Final Version

---
+++ Five Numbers.js
@@ -0,0 +1,38 @@
+function calculations() {
+	var firstNumber = +document.getElementById('firstInput').value;
+	var secondNumber = +document.getElementById('secondInput').value;
+	var thirdNumber = +document.getElementById('thirdInput').value;
+	var fourthNumber = +document.getElementById('fourthInput').value;
+	var fifthNumber = +document.getElementById('fifthInput').value;
+	
+	/*if (firstNumber.length === 0 || secondNumber.length === 0 || thirdNumber.length === 0 || fourthNumber.length === 0 || fifthNumber.length === 0 ) {
+		alert('Please enter a number in each of the boxes!');
+		return;
+	}*/
+	
+	smallestNumber = Math.min(firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber);
+	largestNumber = Math.max(firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber);
+	mySum = firstNumber + secondNumber + thirdNumber + fourthNumber + fifthNumber;
+	myMean = mySum / 5;
+	myProduct = firstNumber * secondNumber * thirdNumber * fourthNumber * fifthNumber;
+	
+	document.getElementById('smallest').innerHTML="The smallest number is: " + smallestNumber;
+	document.getElementById('largest').innerHTML="The largest number is: " + largestNumber;
+	document.getElementById('mean').innerHTML="The mean of the numbers is: " + myMean;
+	document.getElementById('sum').innerHTML="The sum of the numbers is: " + mySum;
+	document.getElementById('product').innerHTML="The product of the numbers is: " + myProduct;			
+}
+
+function myReset() {
+	document.getElementById('firstInput').value = "";
+	document.getElementById('secondInput').value = "";
+	document.getElementById('thirdInput').value = "";
+	document.getElementById('fourthInput').value = "";
+	document.getElementById('fifthInput').value = "";
+	
+	document.getElementById('smallest').innerHTML = "";
+	document.getElementById('largest').innerHTML = "";
+	document.getElementById('mean').innerHTML = "";
+	document.getElementById('sum').innerHTML = "";
+	document.getElementById('product').innerHTML = "";
+}
